cc92197a5fe3bbc328450e5fac06a2e82b09839d
ncerice <nacerice@crimson.ua.edu>
2016-10-04 11:01:21 -0500

    Added SwiftOCR framework to project

--- Pantry Pal/CameraViewController.swift
+++ Pantry Pal/CameraViewController.swift
@@ -1,46 +1,9 @@
 import UIKit
-import AVFoundation
+import SwiftOCR
 
 class CameraViewController: UIViewController {
     
-    let captureSession = AVCaptureSession()
-    
-    // If we find a device we'll store it here for later use
-    var captureDevice : AVCaptureDevice?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        captureSession.sessionPreset = AVCaptureSessionPresetLow
-        
-        let devices = AVCaptureDevice.devices()
-        
-        for device in devices {
-            if (device.hasMediaType(AVMediaTypeVideo)) {
-                if(device.position == AVCaptureDevicePosition.Back) {
-                    captureDevice = device as? AVCaptureDevice
-                }
-            }
-        }
-        if captureDevice != nil {
-            beginSession()
-        }
     }
-    
-    func beginSession() {
-        var err : NSError? = nil
-        captureSession.addInput(AVCaptureDeviceInput(device: captureDevice, error: &err))
-        
-        if err != nil {
-            println("error: \(err?.localizedDescription)")
-        }
-        
-        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        self.view.layer.addSublayer(previewLayer)
-        previewLayer?.frame = self.view.layer.frame
-        captureSession.startRunning()
-        
-        //DOES THIS WORK
-    }
-    
 }
