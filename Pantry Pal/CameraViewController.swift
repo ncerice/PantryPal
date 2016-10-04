3cc1f018275844bd9b608b06be1ea1706b8f7fb8
ncerice <nacerice@crimson.ua.edu>
2016-10-04 11:40:25 -0500

    attempting to get SwiftOCR to recognize sample image

--- Pantry Pal/CameraViewController.swift
+++ Pantry Pal/CameraViewController.swift
@@ -5,5 +5,15 @@ class CameraViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        swiftOCRTest()
+    }
+    
+    func swiftOCRTest() {
+        let sampleImage = OCRImage(named: "SampleReceipt")
+        let swiftOCRInstance = SwiftOCR()
+        swiftOCRInstance.recognize(sampleImage!) {recognizedString in
+            print(recognizedString)
+            print(swiftOCRInstance.currentOCRRecognizedBlobs)
+        }        
     }
 }
