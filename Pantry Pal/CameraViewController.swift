5ba16e80ae6faf7fe91dbab4b8868a815fb233d3
Joshua Burgin <jsburgin@crimson.ua.edu>
2016-11-02 12:58:44 -0500

    working back camera in scan tab

--- Pantry Pal/CameraViewController.swift
+++ Pantry Pal/CameraViewController.swift
@@ -1,46 +1,61 @@
+//
+//  CameraViewController.swift
+//  Pantry Pal
+//
+//  Created by Josh Burgin on 10/11/16.
+//  Copyright © 2016 407. All rights reserved.
+//
+
 import UIKit
 import AVFoundation
 
 class CameraViewController: UIViewController {
     
     let captureSession = AVCaptureSession()
-    
-    // If we find a device we'll store it here for later use
     var captureDevice : AVCaptureDevice?
     
     override func viewDidLoad() {
+    
         super.viewDidLoad()
-
+        
         captureSession.sessionPreset = AVCaptureSessionPresetLow
         
-        let devices = AVCaptureDevice.devices()
-        
-        for device in devices {
-            if (device.hasMediaType(AVMediaTypeVideo)) {
-                if(device.position == AVCaptureDevicePosition.Back) {
-                    captureDevice = device as? AVCaptureDevice
-                }
-            }
-        }
-        if captureDevice != nil {
+        self.captureDevice = AVCaptureDevice.defaultDeviceWithDeviceType(AVCaptureDeviceTypeBuiltInWideAngleCamera, mediaType: AVMediaTypeVideo, position: AVCaptureDevicePosition.Back)
+
+        if (captureDevice != nil) {
             beginSession()
         }
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
     func beginSession() {
-        var err : NSError? = nil
-        captureSession.addInput(AVCaptureDeviceInput(device: captureDevice, error: &err))
-        
-        if err != nil {
-            println("error: \(err?.localizedDescription)")
+        do {
+            try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
+        } catch _ {
+            print("Error")
         }
         
-        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         self.view.layer.addSublayer(previewLayer)
         previewLayer?.frame = self.view.layer.frame
         captureSession.startRunning()
-        
-        //DOES THIS WORK
     }
     
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
