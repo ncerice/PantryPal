8b6f704d33ed65f1833e19fc7027e863b982b7e8
ncerice <nacerice@crimson.ua.edu>
2016-11-15 12:56:11 -0600

    Added initial table view functions

---
+++ Pantry Pal/PantryViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PantryViewController.swift
+//  Pantry Pal
+//
+//  Created by Nathan Cerice on 11/15/16.
+//  Copyright © 2016 407. All rights reserved.
+//
+
+import UIKit
+
+class PantryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 4
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Item")! as UITableViewCell
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Store name - Date"
+    }
+}
